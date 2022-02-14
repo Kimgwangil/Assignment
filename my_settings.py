c4e22f3e722d95c47865b8e68095a255d6fa2837
Kimgwangil <kwangil0803@gmail.com>
2022-02-14 15:19:06 +0900

    assignment ready

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wecode_assignment',
+        'USER': 'root',
+        'PASSWORD': 'amam',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-ss!#*)-k=7%b8z*m79cywk2e@&$59k-8&%-q^q*f9a-%#)k!9d'
